34345ee49b60efd4da5015341d515c8860fa590a
SE-Spark <samuelmunyeke.2@gmail.com>
2021-11-19 17:54:22 +0300

    branch8

--- PSI NET CORE/Network/Mappers/Interfaces/IMapper.cs
+++ PSI NET CORE/Network/Mappers/Interfaces/IMapper.cs
@@ -4,11 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace PSI_NET_CORE.Network.Mappers
-{
-    public interface IMapper<in TInput,out TOutput> 
-    {
-        TOutput Map(TInput input);
-    }
+{   
     public interface IMap<Dto, DomainModel>
     {
         Dto MapToDto(DomainModel m);
